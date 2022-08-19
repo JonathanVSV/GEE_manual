98e86af1772900f122b3c33e0bcf50aab9c5b31f
Jonathan V. Solórzano <JonathanVSV@users.noreply.github.com>
2022-08-19 13:19:56 -0500

    Updated minor changes

--- postProcess/removepages.R
+++ postProcess/removepages.R
@@ -1,6 +1,6 @@
 library(staplr)
 
 # Quitar hojas que no necesitamos
-remove_pages(rmpages = c(1,8,219),
+remove_pages(rmpages = c(1, 8, 217, 219),
              input_filepath = "_book/ManualGEE.pdf",
              output_filepath = "_book/ManualGEE_f.pdf")
