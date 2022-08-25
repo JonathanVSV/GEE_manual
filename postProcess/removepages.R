71a82e7f7eef45761953ab98e6ca15af841f05dc
Jonathan V. Solórzano <JonathanVSV@users.noreply.github.com>
2022-08-25 11:58:28 -0500

    actualizar hoja legal

--- postProcess/removepages.R
+++ postProcess/removepages.R
@@ -1,6 +1,6 @@
 library(staplr)
 
 # Quitar hojas que no necesitamos
-remove_pages(rmpages = c(1, 9, 10, 219, 221),
+remove_pages(rmpages = c(1, 9, 10, 217, 219, 221),
              input_filepath = "_book/ManualGEE.pdf",
              output_filepath = "_book/ManualGEE_f.pdf")
